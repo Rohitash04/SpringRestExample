73e09d544d39bbdeb07d70423d8d4c419d4115cb
Rohitash04 <34044666+Rohitash04@users.noreply.github.com>
2021-05-06 13:08:29 +0100

    Create BookRepositorySpecification.java

---
+++ src/main/java/com/practice/sample/repository/BookRepositorySpecification.java
@@ -0,0 +1,42 @@
+package com.practice.sample.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.jpa.domain.Specification;
+
+// from https://docs.spring.io/spring-data/jpa/docs/1.5.0.RELEASE/reference/html/jpa.repositories.html section 2.4 
+
+public class BookRepositorySpecification implements Specification<BookEntity> {
+
+  private Book book;
+
+  public BookRepositorySpecification(Book book) {
+    this.book = book;
+  }
+
+  @Override
+  public Predicate toPredicate(Root<BookEntity> root, CriteriaQuery<?> cq, CriteriaBuilder builder) {
+    List<Predicate> predicates = new ArrayList<>();
+
+  
+    predicates.add(builder.equal(root.get("id"), book.getId()));
+
+    if (StringUtils.isNotEmpty(book.getName())) {
+      predicates.add(
+        builder.equal(root.get("name"), book.getName()));
+    }
+
+    predicates.add(builder.notEqual(root.get("authorEntity").get("id"), "Undefined"));
+
+    return builder.and(predicates.toArray(new Predicate[0]));
+  }
+}
+
+//To Use
+//BookRepositorySpecification bookRepositorySpecification = new BookRepositorySpecification(payload);
+ //   books = bookRepository.findAll(bookRepositorySpecification);
