303f9308c05758f72c5fa73444049dcb8c15b942
Rohitash04 <34044666+Rohitash04@users.noreply.github.com>
2021-09-20 12:01:56 +0100

    Update Author.java

--- src/main/java/com/practice/sample/model/Author.java
+++ src/main/java/com/practice/sample/model/Author.java
@@ -12,4 +12,13 @@ public class Author {
     long id;
     String name;
     Address address;
+    
+    //latest book by this author using @JoinFormule
+    /**
+    @ManyToOne(fetch - FetchType.EAGER)
+    @JoinFormule(
+        "(SELECT B.ID FROM BOOK B WHERE B.AUTHOR_ID = ID and B.CREATE_TS = (select max(BK.CREATE_TS) From BOOK BK where BK.AUTHOR_ID = ID))"
+    )
+    private Book book;
+    **/
 }
